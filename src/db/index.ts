7bd32dadc4cf8afdd1c7b3c761f2bb3f4ce7a87c
nasir21b@gmail.com <nasir21b@gmail.com>
2026-05-23 04:16:00 +0600

    feat: Create initial database tables for users and issues

--- src/db/index.ts
+++ src/db/index.ts
@@ -9,8 +9,33 @@ export const db = new Pool({
 
 export const initDB = async () => {
   try {
-    await db.query("SELECT NOW()");
-    console.log("Database Connected successfully ");
+    await db.query(`
+    CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(150) NOT NULL,
+    role VARCHAR(20) DEFAULT 'contributor' CHECK (role IN ('contributor', 'maintainer')),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+
+    await db.query(`
+    CREATE TABLE IF NOT EXISTS issues (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(150) NOT NULL,
+    description TEXT NOT NULL,
+    type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+    status VARCHAR(20) DEFAULT 'open' CHECK (status IN ('open', 'in_progress', 'resolved')),
+    reporter_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  );
+`);
+
+
+    console.log("User and Issues Table Created successfully on DB");
   } catch (error) {
     console.error("Database not connected", error);
   }
--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,12 @@
 import express, { type Request, type Response } from "express";
 import config from "./config";
 import { initDB } from "./db";
+import app from "./app";
 
-const app = express();
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("DevPules Server is runing on port 5000");
-});
-
-const main = ()=>{
+const main = () => {
   app.listen(config.port, () => {
-  console.log(`Server listening on port ${config.port}`);
-});
-}
-initDB()
-main()
-
-
-
+    console.log(`Server listening on port ${config.port}`);
+  });
+};
+initDB();
+main();
